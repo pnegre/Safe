20f27a9b2060aab190a295082351224cf37f05bf
Pere Negre <pere.negre@gmail.com>
2012-01-03 22:15:13 +0100

    First commit

---
+++ src/com/pnegre/safe/SafeActivity.java
@@ -0,0 +1,15 @@
+package com.pnegre.safe;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SafeActivity extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
