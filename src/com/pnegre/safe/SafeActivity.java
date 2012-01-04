afdd662f97e9afb6a2e54fbb3f6f1fbe0b67c668
Pere Negre <pere.negre@gmail.com>
2012-01-04 01:36:08 +0100

    Anem fent

--- src/com/pnegre/safe/SafeActivity.java
+++ src/com/pnegre/safe/SafeActivity.java
@@ -9,6 +9,7 @@ import android.view.View;
 
 import android.widget.EditText;
 import android.app.AlertDialog;
+import android.content.DialogInterface;
 
 public class SafeActivity extends ListActivity
 {
@@ -23,6 +24,26 @@ public class SafeActivity extends ListActivity
 		ArrayAdapter<Secret> adapter = new ArrayAdapter<Secret>(this, android.R.layout.simple_list_item_1, secrets);
 		setListAdapter(adapter);
 	
+	}
+	
+	public void onResume()
+	{
+		super.onResume();
+		AlertDialog.Builder alert = new AlertDialog.Builder(this);                 
+		alert.setTitle("Password");
+		alert.setPositiveButton("OK", new DialogInterface.OnClickListener() {  
+				public void onClick(DialogInterface dialog, int whichButton) {  
+			}
+		});
+		alert.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {  
+				public void onClick(DialogInterface dialog, int whichButton) {  
+			}
+		});
+
+		// Set an EditText view to get user input   
+		final EditText input = new EditText(this); 
+		alert.setView(input);
+		alert.show();
 	}
 
 	@Override
