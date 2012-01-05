a4a2fe2f96eb4d3178031b78b1ac4736b48d1f4e
Pere Negre <pere.negre@gmail.com>
2012-01-05 01:28:09 +0100

    Anem fent

---
+++ src/com/pnegre/safe/NewSecretActivity.java
@@ -0,0 +1,14 @@
+package com.pnegre.safe;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class NewSecretActivity extends Activity
+{
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+	}
+}
