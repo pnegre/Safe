d460bbd709476d38903f96e489fce23f72917971
Pere Negre <pere.negre@gmail.com>
2012-01-05 12:26:40 +0100

    Afegida classe SafeApp

--- src/com/pnegre/safe/NewSecretActivity.java
+++ src/com/pnegre/safe/NewSecretActivity.java
@@ -2,13 +2,35 @@ package com.pnegre.safe;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.widget.EditText;
+import android.widget.Button;
+import android.view.View;
 
 public class NewSecretActivity extends Activity
 {
+	private EditText sitenameET, siteusnameET, sitepasswordET;
+	private Button newsecretBT;
+	
 	/** Called when the activity is first created. */
 	@Override
 	public void onCreate(Bundle savedInstanceState)
 	{
 		super.onCreate(savedInstanceState);
+		setContentView(R.layout.newsecret);
+		
+		sitenameET = (EditText) findViewById(R.id.sitename);
+		siteusnameET = (EditText) findViewById(R.id.siteusname);
+		sitepasswordET = (EditText) findViewById(R.id.sitepassword);
+		
+		newsecretBT = (Button) findViewById(R.id.butnewsecret);
+		newsecretBT.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				newSecret();
+			}
+		});
+	}
+	
+	void newSecret()
+	{
 	}
 }
---
+++ src/com/pnegre/safe/SafeApp.java
@@ -0,0 +1,10 @@
+package com.pnegre.safe;
+
+import android.app.Application;
+
+public class SafeApp extends Application
+{
+	public void onCreate()
+	{
+	}
+}
