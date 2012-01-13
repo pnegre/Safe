3f021cb19ea94ededf747975a1c80979bed73b08
Pere Negre <pere.negre@gmail.com>
2012-01-13 23:34:02 +0100

    Afegit checkbox per veure efectivament la password

--- src/com/pnegre/safe/ShowSecretActivity.java
+++ src/com/pnegre/safe/ShowSecretActivity.java
@@ -5,13 +5,20 @@ import android.app.Activity;
 import android.widget.TextView;
 import android.widget.Button;
 import android.view.View;
+import android.widget.CheckBox;
 
 
 public class ShowSecretActivity extends Activity
 {
+	private boolean showPassword = false;
 	private SafeApp app;
 	private Database database;
+	private Secret theSecret;
+	private TextView tname;
+	private TextView tusname;
+	private TextView tpw;
 	private Button butDel;
+	private CheckBox cbShowpw;
 	
 	/** Called when the activity is first created. */
 	@Override
@@ -26,13 +33,20 @@ public class ShowSecretActivity extends Activity
 		{
 			Bundle extras = getIntent().getExtras();
 			final int secretId = extras.getInt("secretid");
-			Secret s = database.getSecret(secretId);
-			TextView tname = (TextView) findViewById(R.id.secretsitename);
-			tname.setText(s.name);
-			TextView tusname = (TextView) findViewById(R.id.secretsiteusname);
-			tusname.setText(s.username);
-			TextView tpw = (TextView) findViewById(R.id.secretsitepw);
-			tpw.setText(s.password);
+			theSecret = database.getSecret(secretId);
+			tname = (TextView) findViewById(R.id.secretsitename);
+			tname.setText(theSecret.name);
+			tusname = (TextView) findViewById(R.id.secretsiteusname);
+			tusname.setText(theSecret.username);
+			tpw = (TextView) findViewById(R.id.secretsitepw);
+			showPasswordFlip();
+			
+			cbShowpw = (CheckBox) findViewById(R.id.checkpw);
+			cbShowpw.setOnClickListener(new View.OnClickListener() {
+				public void onClick(View v) {
+					showPasswordFlip();
+				}
+			});
 			
 			butDel = (Button) findViewById(R.id.butdelsecret);
 			butDel.setOnClickListener(new View.OnClickListener() {
@@ -46,5 +60,17 @@ public class ShowSecretActivity extends Activity
 			});
 		} 
 		catch (Exception e) { }
+	}
+	
+	// Mostra "XXXXXXXX" o la password, cada vegada que es crida la funció
+	// O sigui, va alternant.
+	void showPasswordFlip()
+	{
+		if (showPassword == false)
+			tpw.setText("XXXXXXXX");
+		else
+			tpw.setText(theSecret.password);
+		
+		showPassword = ! showPassword;
 	}
 }
