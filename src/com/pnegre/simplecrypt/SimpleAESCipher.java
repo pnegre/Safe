fae466f79c3c1fd9c5e12f359c07a22293a16324
Pere Negre <pere.negre@gmail.com>
2012-10-07 21:51:52 +0200

    Provant AES/CBC

--- src/com/pnegre/simplecrypt/SimpleAESCipher.java
+++ src/com/pnegre/simplecrypt/SimpleAESCipher.java
@@ -10,9 +10,12 @@ import javax.crypto.BadPaddingException;
 import javax.crypto.Cipher;
 import javax.crypto.IllegalBlockSizeException;
 import javax.crypto.NoSuchPaddingException;
+import javax.crypto.spec.IvParameterSpec;
+import java.security.InvalidAlgorithmParameterException;
 import java.security.InvalidKeyException;
 import java.security.Key;
 import java.security.NoSuchAlgorithmException;
+import java.util.Random;
 
 /**
  * Cipher simplificat (transforma les excepcions en runtime)
@@ -24,7 +27,7 @@ class SimpleAESCipher {
     SimpleAESCipher(Key k) {
         try {
             key = k;
-            cipher = Cipher.getInstance("AES");
+            cipher = Cipher.getInstance("AES/CBC/PKCS5PADDING"); // AES in CBC mode (stream cipher)
         } catch (NoSuchAlgorithmException e) {
             throw new RuntimeException();
         } catch (NoSuchPaddingException e) {
@@ -34,8 +37,29 @@ class SimpleAESCipher {
 
     void init(int mode) {
         try {
-            cipher.init(mode, key);
+            byte[] iv = new byte[16];
+
+            iv[0] = 10;
+            iv[1] = 12;
+            iv[2] = 40;
+            iv[3] = 17;
+            iv[4] = 17;
+            iv[5] = 120;
+            iv[6] = 120;
+            iv[7] = 110;
+            iv[8] = -3;
+            iv[9] = 5;
+            iv[10] = 120;
+            iv[11] = 120;
+            iv[12] = 110;
+            iv[13] = -3;
+            iv[14] = 5;
+            iv[15] = 120;
+
+            cipher.init(mode, key, new IvParameterSpec(iv));
         } catch (InvalidKeyException e) {
+            throw new RuntimeException();
+        } catch (InvalidAlgorithmParameterException e) {
             throw new RuntimeException();
         }
     }
