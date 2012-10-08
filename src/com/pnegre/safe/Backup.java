157d7e338110a8a1af5772e7a05f2081dce47875
Pere Negre <pere.negre@gmail.com>
2012-10-08 22:35:55 +0200

    Treballant en import/export...

--- src/com/pnegre/safe/Backup.java
+++ src/com/pnegre/safe/Backup.java
@@ -5,7 +5,7 @@ import android.util.Log;
 import com.pnegre.safe.database.Database;
 import com.pnegre.safe.database.Secret;
 import com.pnegre.simplecrypt.SimpleAESCryptCBC;
-import com.pnegre.simplecrypt.SimpleAESCryptECB;
+import net.iharder.base64.Base64;
 import org.w3c.dom.*;
 
 import javax.xml.parsers.DocumentBuilder;
@@ -19,6 +19,7 @@ import javax.xml.transform.stream.StreamResult;
 import javax.xml.transform.stream.StreamSource;
 import java.io.*;
 import java.util.List;
+import java.util.Random;
 import java.util.Set;
 import java.util.TreeSet;
 
@@ -35,6 +36,26 @@ class Backup {
         56,16,-5,4,-3,-48,122,21
     };
 
+    private static byte[] randomIV() {
+        byte[] result = new byte[16];
+        Random r = new Random();
+        for (int i=0;i<16;i++) {
+            result[i] = (byte) (r.nextInt(256) - 127);
+        }
+        return result;
+    }
+
+    private static void copyStream(InputStream input, OutputStream output)
+            throws IOException
+    {
+        byte[] buffer = new byte[1024]; // Adjust if you want
+        int bytesRead;
+        while ((bytesRead = input.read(buffer)) != -1)
+        {
+            output.write(buffer, 0, bytesRead);
+        }
+    }
+
 
     Backup(Database dataBase) {
         this.dataBase = dataBase;
@@ -42,7 +63,43 @@ class Backup {
 
     // Exporta els secrets i torna el nom del fitxer gravat
     String doExport(String password) throws Exception {
+        byte[] iv = randomIV();
+        SimpleAESCryptCBC simpleCrypt = new SimpleAESCryptCBC(password.getBytes(), iv);
+        String iv_b64 = Base64.encodeBytes(iv);
 
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        OutputStream os = simpleCrypt.cryptedOutputStream(baos);
+        ByteArrayInputStream bais = new ByteArrayInputStream(getSecretsXMLString().getBytes());
+        copyStream(bais,os);
+        String data_b64 = Base64.encodeBytes(baos.toByteArray());
+
+        DocumentBuilder db = DocumentBuilderFactory.newInstance().newDocumentBuilder();
+        Document doc = db.newDocument();
+        Element rootElement = doc.createElement("data");
+        doc.appendChild(rootElement);
+
+        Element e = doc.createElement("iv");
+        rootElement.appendChild(e);
+        e.appendChild(doc.createTextNode(iv_b64));
+
+        e = doc.createElement("xmldata");
+        rootElement.appendChild(e);
+        e.appendChild(doc.createTextNode(data_b64));
+
+        Transformer transformer = TransformerFactory.newInstance().newTransformer();
+        transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+
+        File dir = getSafeDirectory();
+        String filename = String.format("safe.backup.crypt.%d.xml", System.currentTimeMillis());
+        Log.v(SafeApp.LOG_TAG, "Writing " + filename);
+        File file = new File(dir, filename);
+
+        transformer.transform(new DOMSource(doc), new StreamResult(new FileOutputStream(file)));
+        return filename;
+    }
+
+
+    private String getSecretsXMLString() throws Exception {
         DocumentBuilder db = DocumentBuilderFactory.newInstance().newDocumentBuilder();
         Document doc = db.newDocument();
         Element rootElement = doc.createElement("data");
@@ -69,17 +126,12 @@ class Backup {
         Transformer transformer = TransformerFactory.newInstance().newTransformer();
         transformer.setOutputProperty(OutputKeys.INDENT, "yes");
 
-        File dir = getSafeDirectory();
-        String filename = String.format("safe.backup.crypt.%d", System.currentTimeMillis());
-        Log.v(SafeApp.LOG_TAG, "Writing " + filename);
-        File file = new File(dir, filename);
-        SimpleAESCryptCBC simpleCrypt = new SimpleAESCryptCBC(password.getBytes(), iv);
-        OutputStream os = simpleCrypt.cryptedOutputStream(new FileOutputStream(file));
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        transformer.transform(new DOMSource(doc), new StreamResult(baos));
 
-        transformer.transform(new DOMSource(doc), new StreamResult(os));
-        os.close();
-        return filename;
+        return baos.toString();
     }
+
 
     void doImport(String filename, String password) throws Exception {
         File dir = getSafeDirectory();
@@ -89,8 +141,16 @@ class Backup {
 
         DocumentBuilder db = DocumentBuilderFactory.newInstance().newDocumentBuilder();
         Document doc = db.newDocument();
+
         Transformer transformer = TransformerFactory.newInstance().newTransformer();
         transformer.transform(new StreamSource(is), new DOMResult(doc));
+
+
+        doRealImport(doc);
+
+    }
+
+    private void doRealImport(Document doc) {
         Node root = doc.getFirstChild();
         if (!root.getNodeName().equals("data")) throw new RuntimeException();
 
@@ -123,6 +183,7 @@ class Backup {
                     dataBase.newSecret(s);
             }
         }
+
     }
 
     /**
